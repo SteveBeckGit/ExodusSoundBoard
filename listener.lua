29c3b8b290898e03db7138192f5da014b4fb94d1
Steve Beck <sbeck@dvt.co.za>
2019-04-27 20:40:09 +0200

    guild listener added

--- listener.lua
+++ listener.lua
@@ -2,6 +2,7 @@ local frame=CreateFrame("Frame");-- Need a frame to capture events
 frame:RegisterEvent("CHAT_MSG_RAID");
 frame:RegisterEvent("CHAT_MSG_PARTY_LEADER");
 frame:RegisterEvent("CHAT_MSG_RAID_LEADER");
+frame:RegisterEvent("CHAT_MSG_GUILD");
 frame:RegisterEvent("CHAT_MSG_PARTY");-- Register our event
 frame:SetScript("OnEvent",function(self,event,msg)-- OnEvent handler receives event triggers
     if (event=="CHAT_MSG_RAID" or event=="CHAT_MSG_GUILD" or event=="CHAT_MSG_PARTY" or event =="CHAT_MSG_BN" or event=="CHAT_MSG_RAID_LEADER" or event=="CHAT_MSG_PARTY_LEADER") and msg=="Wed Wench" then-- Someone sends "Wed Wench" in /raid
