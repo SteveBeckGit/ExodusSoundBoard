ae6e25f4f0507d22359bcc8027d9a22ac725792a
Steve Beck <sbeck@dvt.co.za>
2019-04-28 12:56:57 +0200

    Removed buttons. Added listeners

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
-local f = CreateFrame("Frame")
-f:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end)
-f:RegisterEvent("CHAT_MSG_WHISPER")
+
 -- setting up slash commands and show/hide functionality
 SLASH_ESB1 = "/ESB"
 SlashCmdList["ESB"] = function(msg)
@@ -15,21 +13,7 @@ SlashCmdList["ESB"] = function(msg)
 
 
   
-  function f:CHAT_MSG_WHISPER(msg)
-    -- PlaySound(5274)
-    local color = {r = 0, g = 1, b = 0}
-    if msg == "WarrenKappa" then
-      RaidNotice_AddMessage(RaidWarningFrame, "OOOPH", color)
-      PlaySoundFile("Interface\\AddOns\\ExodusSoundBoard\\Sounds\\2_warrenKappa.ogg","Master")
-      
-    end
-
-    if msg == "Wed Wench" then
-      RaidNotice_AddMessage(RaidWarningFrame, "Wed Wench", color)
-      PlaySoundFile("Interface\\AddOns\\ExodusSoundBoard\\Sounds\\1_red_wrench.ogg")
-    end
-   
-  end
+ 
 
   
   
