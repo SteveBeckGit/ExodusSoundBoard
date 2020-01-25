b6208f534c907f435f21ad8790c199e900a784fd
Steve Beck <sbeck@dvt.co.za>
2020-01-25 13:17:25 +0200

    Added /ESB command to print info message to console

--- main.lua
+++ main.lua
@@ -2,13 +2,19 @@
 -- setting up slash commands and show/hide functionality
 SLASH_ESB1 = "/ESB"
 SlashCmdList["ESB"] = function(msg)
-    if (PutItInFrame:IsShown()) then
-      PutItInFrame:Hide()
+    print("<ESB> Welcome to Exodus Sound Board")
+    print("<ESB> Each command is case sensitive, but can be included in a phrase")
+    print("<ESB> The following commands are included in this release:")
+    print("<ESB> Wed Wench")
+    print("<ESB> KappaWarren")
+    print("<ESB> KiltPride")
+    print("<ESB> Blue")
+    print("<ESB> HowManyWeeks")
+    print("<ESB> NotTheBelt")
+    print("<ESB> Bruce")
+    print("<ESB> Mana")
+
    
-    else
-      PutItInFrame:Show()
-     
-    end
   end 
 
 
